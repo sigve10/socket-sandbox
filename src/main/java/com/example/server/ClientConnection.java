c5791c71dcf5cb6d55318a1c3664806842df8ed2
sigvebjo <sigve@bjorkedal.org>
2023-10-27 15:02:15 +0200

    Fixed reply issues

--- src/main/java/com/example/server/ClientConnection.java
+++ src/main/java/com/example/server/ClientConnection.java
@@ -1,8 +1,6 @@
 package com.example.server;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -53,7 +51,6 @@ public class ClientConnection extends Thread {
 		try {
 			String rawMessage = input.readLine();
 			System.out.println(" >>> " + rawMessage);
-			reply(rawMessage);
 
 			message = this.commandSet.tryToExecuteCommand(rawMessage);
 		} catch (IOException e) {
