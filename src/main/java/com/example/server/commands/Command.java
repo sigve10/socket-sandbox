509b90fa231ba2d72115684585e422cc156c4dd5
sigvebjo <sigve@bjorkedal.org>
2023-10-27 12:46:33 +0200

    Added logic interface, JavaDoc

--- src/main/java/com/example/server/commands/Command.java
+++ src/main/java/com/example/server/commands/Command.java
@@ -1,13 +1,29 @@
 package com.example.server.commands;
 
+import com.example.handler.Logic;
+
+/**
+ * Abstract class responsible for executing commands.
+ */
 public abstract class Command {
 	protected int argumentCount;
 	protected String[] args;
 	
+	/**
+	 * Creates a command with a fixed number of arguments
+	 * @param argumentCount the number of arguments
+	 */
 	protected Command(int argumentCount) {
 		this.argumentCount = argumentCount;
 		this.args = new String[argumentCount];
 	}
 
-	public abstract String execute(String[] args);
+	/**
+	 * Executes this commmand.
+	 *
+	 * @param actor an object on which to perform the command on (can be null)
+	 * @param args an array of arguments to run the command with
+	 * @return a string response from the command
+	 */
+	public abstract String execute(Logic actor, String[] args);
 }
--- src/main/java/com/example/server/commands/CommandSet.java
+++ src/main/java/com/example/server/commands/CommandSet.java
@@ -3,6 +3,8 @@ package com.example.server.commands;
 import java.util.HashMap;
 import java.util.Iterator;
 
+import com.example.handler.Logic;
+
 public class CommandSet {
 	public static final char COMMAND_SEPARATOR = ' ';
 	private HashMap<String, Command> commands;
@@ -11,7 +13,7 @@ public class CommandSet {
 		this.commands = builder.commands;
 	}
 
-	public String tryToExecuteCommand(String rawCommand) {
+	public String tryToExecuteCommand(Logic actor, String rawCommand) {
 		String[] commandContents = rawCommand.split(String.valueOf(COMMAND_SEPARATOR));
 		String commandWord = commandContents[0];
 		Iterator<String> iterator = commands.keySet().iterator();
@@ -24,7 +26,7 @@ public class CommandSet {
 			
 			if (commandWord.matches(current)) {
 				command = commands.get(current);
-				result = command.execute(commandContents);
+				result = command.execute(actor, commandContents);
 			}
 		}
 
