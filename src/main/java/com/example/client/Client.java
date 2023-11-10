adcbdc825f515bf360fd51d3ed88b519ecbe7bab
sigvebjo <sigve@bjorkedal.org>
2023-11-10 10:38:19 +0100

    Client continuously listens
    The client now continuously listens to a server.
    Also is no longer dependent on a hardcoded input stream.

--- src/main/java/com/example/client/Client.java
+++ src/main/java/com/example/client/Client.java
@@ -5,120 +5,73 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
+import java.util.LinkedList;
 
 /**
- * Here is the javadoc for the client :)
+ * A client connection to a server. Capable of continuously reading information from the client and
+ * sending messages.
+ * 
+ * @see Client#sendOutgoingMessage(String) sendOutgoingMessage
+ * @see Client#nextIncomingMessage() nextIncomingMessage
+ * 
+ * @author Sigve Bjørkedal
  */
 public class Client {
-	private Socket socket;
-	private BufferedReader input;
+	private LinkedList<String> incomingMessages;
 	private PrintWriter output;
-	private BufferedReader replyInput;
+	private Socket socket;
 
 	/**
-	 * Constructor for the Client
+	 * Creates a new client connection to a server.
 	 * 
-	 * @param address 
-	 * @param port Port the client will connect to
-	 * @throws IOException if shit hits the fan
+	 * @param address the address of the server to connect to
+	 * @param port the port of the server to connect to
+	 * @throws IOException if connecting to the server fails
 	 */
 	public Client(String address, int port) throws IOException {
-		try {
-			this.socket = new Socket(address, port);
-			System.out.println("Client: Connected to " + address + ":" + port);
-			this.input = new BufferedReader(new InputStreamReader(System.in));
-			this.output = new PrintWriter(socket.getOutputStream(), true);
-			this.replyInput = new BufferedReader(
-				new InputStreamReader(
-					socket.getInputStream()
-				)
-			);
-		} catch(UnknownHostException e) {
-			System.err.println("Could not connect to " + address + ":" + port);
-		}
-		startReadingInput();
+		this.incomingMessages = new LinkedList<>();
+		this.socket =  new Socket(address, port);
+
+		BufferedReader socketResponseStream = new BufferedReader(
+			new InputStreamReader(
+				this.socket.getInputStream()));
+
+		this.output = new PrintWriter(this.socket.getOutputStream(), true);
+
+		new ClientListener(this, socketResponseStream).start();
 	}
 
 	/**
-	 * Reads input from command line
+	 * Sends a message to the server.
+	 *
+	 * @param message message to send to the server.
 	 */
-	private void startReadingInput() {
-		String message = null;
-		do {
-			try {
-				message = this.input.readLine();
-				sendMessage(message);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		} while (message == null || !message.equalsIgnoreCase("disconnect"));
-		close();
+	public void sendOutgoingMessage(String message) {
+		this.output.println(message);
 	}
 
 	/**
-	 * sends a message to the server
+	 * Attempts to retrieve the earliest received message from the server.
+	 * 
+	 * @return the earliest received message, or null if it does not exist.
 	 */
-	private void sendMessage(String messsage){
-		output.println(messsage);
-		listenForReply();
+	public String nextIncomingMessage() {
+		String retval = null;
+
+		if (this.incomingMessages.peek() != null) {
+			retval = this.incomingMessages.getFirst();
+		}
+
+		return retval;
 	}
 
 	/**
-	 * Closes the connection.
+	 * Registers a new message to the client. Can be read through {@link Client#nextIncomingMessage
+	 * nextIncomingMessage}.
+	 *
+	 * @param message the message to register.
 	 */
-	private void close() {
-		try {
-			this.input.close();
-			this.output.close();
-			this.socket.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
+	public void registerIncomingMessage(String message) {
+		this.incomingMessages.add(message);
 	}
-
-	/**
-	 * Listens for a reply from the server
-	 */
-	private void listenForReply() {
-		String message = null;
-
-		try {
-			message = replyInput.readLine();
-			System.out.println("Recived reply " + message);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}	
-	
-	public static void main(String args[]) {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		int maxAttempts = 3;
-	
-		for (int i = 0; i < maxAttempts; i++) {
-			try {
-				System.out.print("Enter server address (default: localhost): ");
-				String address = reader.readLine().trim();
-				if (address.isEmpty()) {
-					address = "localhost";
-				}
-	
-				System.out.print("Enter server port (default: 8080): ");
-				String portStr = reader.readLine().trim();
-				int port = portStr.isEmpty() ? 8080 : Integer.parseInt(portStr);
-	
-				Client client = new Client(address, port);
-				break; 
-			} catch (IOException e) {
-				if (i < maxAttempts - 1) {
-					System.out.println("Failed to connect. Retrying (" + (i + 2) + "/" + maxAttempts + ")...");
-				} else {
-					System.out.println("Failed to connect after " + maxAttempts + " attempts.");
-					e.printStackTrace();
-				}
-			}
-		}
-	}
-	
 }
---
+++ src/main/java/com/example/client/ClientListener.java
@@ -0,0 +1,62 @@
+package com.example.client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+/**
+ * A listener thread for a client connection. Continuously checks for new messages from a server
+ * and sends them to the {@link Client}.
+ * 
+ * @author Sigve Bjørkedal
+ */
+public class ClientListener extends Thread {
+	Client client;
+	BufferedReader messageStream;
+
+	/**
+	 * Creates a new listening thread based on a client connection.
+	 *
+	 * @param client the client this connection belongs to
+	 * @param messageStream the input the listener should listen to
+	 */
+	public ClientListener(Client client, BufferedReader messageStream) {
+		this.client = client;
+		this.messageStream = messageStream;
+	}
+
+	@Override
+	public void run() {
+		String incomingMessage = null;
+
+		do {
+			try {
+				incomingMessage = messageStream.readLine();
+				handleIncomingMessage(incomingMessage);
+			} catch (IOException e) {
+				System.err.println("Failed to receive input: " + e.getMessage());
+			}
+		} while (incomingMessage == null);
+
+		closeInput();
+	}
+
+	/**
+	 * Registers an incoming message to the client
+	 *
+	 * @param message message to register
+	 */
+	private synchronized void handleIncomingMessage(String message) {
+		this.client.registerIncomingMessage(message);
+	}
+
+	/**
+	 * Attempts to close the connection
+	 */
+	private void closeInput() {
+		try {
+			this.messageStream.close();
+		} catch (IOException e) {
+			System.err.println("Failed to close input stream: " + e.getMessage());
+		}
+	}
+}
